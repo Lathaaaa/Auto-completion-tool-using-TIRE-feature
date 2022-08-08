8b56e49ffe822910556c1cc7dfdf6e006d3d3c4b
Lathaaaa <97357542+Lathaaaa@users.noreply.github.com>
2022-08-09 00:09:57 +0530

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,41 @@
+const searchBtn = document.getElementById("btn-search");
+const searchInput = document.getElementById("input-search");
+const item = document.getElementById("item");
+
+// make a trie and add countries
+const root = new newNode("\0");
+for (const galaxy of Galaxylist) {
+  insertWord(galaxy, 0, root);
+}
+
+//event listner for displaying search bar
+searchBtn.addEventListener("click", () => {
+  searchInput.classList.add("search");
+});
+
+// event listner for displaying suggestion list
+searchInput.addEventListener("keyup", (e) => {
+  const value = capitalizeFirstLetter(e.target.value);
+
+  // searching words in trie
+  const resultWords = searchWord(value, 0, root);
+  const countWords = resultWords.length;
+
+  // add to ul
+  item.innerHTML = "";
+  if (countWords > 5) {
+    for (let i = 0; i < 5; i++) {
+      item.innerHTML += `<li onclick="handleClick(this)" >${resultWords[i]}</li>`;
+    }
+  } else {
+    for (let i = 0; i < countWords; i++) {
+      item.innerHTML += `<li onclick="handleClick(this)" >${resultWords[i]}</li>`;
+    }
+  }
+});
+
+function handleClick(e) {
+  const text = e.innerHTML;
+  searchInput.value = text;
+  item.innerHTML = "";
+}
